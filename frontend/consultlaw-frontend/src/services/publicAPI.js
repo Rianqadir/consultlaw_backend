b0ac08d54b32f73f794c35437119c538c152e9dc
Rian Qadir <rianqadir2011@gmail.com>
2025-07-28 22:09:03 +0500

    Added my-bookings route for clients

--- frontend/consultlaw-frontend/src/services/publicAPI.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const publicAPI = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://consultlaw-backend.onrender.com/api/auth/',
-});
-
-export default publicAPI;
