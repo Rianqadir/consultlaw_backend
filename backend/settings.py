d0446c0964cb77a88d3fa5c562248b4a11736111
Rian Qadir <rianqadir2011@gmail.com>
2025-07-28 15:33:27 +0500

    Render deployment ready

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,8 @@ SECRET_KEY = 'django-insecure-iv#akh)vhus2nyb61#tue(eyunwh@r0g1h-s%3z!uismp4m%fl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.render.com']
+
 
 
 # Application definition
@@ -152,3 +156,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+DATABASES['default'] = dj_database_url.config(default=os.getenv('DATABASE_URL'))
